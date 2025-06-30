e7fb4ca30280a83ddfeab6349ccd8b6c65682144
gleniodev <gleniodev@gmail.com>
2025-06-30 00:27:45 -0300

    merge de refactor-modularization

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
-import { MessageListener } from './bot/listeners/message.listener';
 import { BotService } from './bot/bot.service';
 import { SyncUsersModule } from './bot/sync/sync-users.module';
+import { BotListenersModule } from './bot/listeners/bot-listeners.module'; // 👈 Adicione esta linha
 
 @Module({
-  imports: [SyncUsersModule],
-  providers: [BotService, MessageListener, PrismaService],
+  imports: [
+    SyncUsersModule,
+    BotListenersModule, // 👈 Adicione esta linha
+  ],
+  providers: [BotService, PrismaService],
+  // Remova MessageListener daqui, pois agora está no BotListenersModule
 })
 export class AppModule {}
